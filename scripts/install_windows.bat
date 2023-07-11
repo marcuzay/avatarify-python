ca57f94e732a11caa93c71657c0a25a728a57637
marcuzay <68786444+marcuzay@users.noreply.github.com>
2023-07-11 21:16:05 +0100

    Update install_windows.bat

--- scripts/install_windows.bat
+++ scripts/install_windows.bat
@@ -9,10 +9,10 @@ call scripts/settings_windows.bat
 call conda create -y -n %CONDA_ENV_NAME% python=3.10
 call conda activate %CONDA_ENV_NAME%
 
+call pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 call mamba install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia -c conda-forge
 call conda install -y scikit-image python-blosc -c conda-forge
 call conda install -y -c anaconda git
-call pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 call conda install cudatoolkit=11.8
 
 REM ###FOMM###
